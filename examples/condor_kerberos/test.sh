1b00f43bb2ee8c12be3519a3a185de3324ed5459
Angel de Vicente <angel.de.vicente@iac.es>
2023-02-28 09:24:38 +0000

    Final version?
    eight hour job (8 loops), submitted to each machine in the condor pool 25 times,
    enough to cover a whole week. Let's wait and see if we get errors

--- examples/condor_kerberos/test.sh
+++ examples/condor_kerberos/test.sh
@@ -1,46 +1,46 @@
 #!/usr/bin/env bash
 
 uname=`whoami`
-
 echo running in `uname -a`
-echo cache: $KRB5CCNAME
-#export KRB5CCNAME=/tmp/krb5cc_${UID}_$3
-rm /scratch/.krb5cc_2071_*
-kinit -V ${uname}@IAC.ES -k -t krb_keytab -c $KRB5CCNAME
 
-# Create a new ticket every 28800s == 8 hours
-(while true; do sleep 28800 ; kinit ${uname}@IAC.ES -k -t krb_keytab -c $KRB5CCNAME ; done) &
+kinit ${uname}@IAC.ES -k -t krb_keytab -c $KRB5CCNAME
+
+# Create a new ticket every 14400s == 4 hours
+(while true; do sleep 14400 ; kinit ${uname}@IAC.ES -k -t krb_keytab -c $KRB5CCNAME ; done) &
 ticket_PID=$!
 
-#sleep 10
-#kinit -V ${uname}@IAC.ES -R -c $KRB5CCNAME
-
-sleep 10
-
-# 24 hour job
-for i in `seq 1 2`
+# 8 hour job
+for i in `seq 1 8`
 do
     echo "============== Loop $i =============="
-    echo ""
 
-    #    ls -ltr krb_keytab
+    echo ""
+    echo "cache"
+    echo "----------"    
     ls -ltr /scratch/.krb5cc_2071*
+
+    echo ""
+    echo "ticket"
+    echo "----------"    
     klist 
 
+    echo ""
+    echo "files"
+    echo "----------"    
     ls -ltr /home/angelv/words*
-#    ls -ltr /net/cacao/scratch/angelv/
     ls -ltr /net/dejar/scratch/angelv/words*
-    
-    ./simple $1 $2
 
-#    echo "connecting by ssh to diablos"
-#    ssh diablos date
+    echo ""
+    echo "executable"
+    echo "----------"
+    ./simple $1 $2
+    echo ""
     
     # 3600 == 1 hour
-    sleep 1
+    sleep 3600
 done
 
 kill $ticket_PID
-wait $ticket_PID # 2>/dev/null
-# rm -f /scratch/krb5cc_${UID}
+wait $ticket_PID 2>/dev/null
+
 
