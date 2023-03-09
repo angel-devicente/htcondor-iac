4db651b7561795c8f251ec93a6690b783255d2cb
Angel de Vicente <angel.de.vicente@iac.es>
2023-03-09 16:54:14 +0000

    Kerberos recipe finished

--- recipes/condor_kerberos/kerberos.sh
+++ recipes/condor_kerberos/kerberos.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 ######### DO NOT EDIT - get Kerberos Tickets
+uname=`whoami` 
 kinit ${uname}@IAC.ES -k -t krb_keytab -c $KRB5CCNAME
 
 # Create a new ticket every 14400s == 4 hours
 (while true; do sleep 14400 ; kinit ${uname}@IAC.ES -k -t krb_keytab -c $KRB5CCNAME ; done) &
 ticket_PID=$!
-######### 
+######### END
 
 
 ./simple $1 $2
+ls -lt /home/${uname}/
 
 
 ######### DO NOT EDIT - clean up after job is finished
@@ -17,4 +19,4 @@ kill $ticket_PID
 wait $ticket_PID 2>/dev/null
 
 exit 0
-######### 
+######### END
