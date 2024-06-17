da1c75ff940ffe14345daf67cbca76166d36a25e
Angel de Vicente <angel.de.vicente@iac.es>
2024-06-17 13:34:03 +0100

    Explicitly export PATH
    We don't understand why, but we now explicitly export the PATH or the system
    paths will be removed when initializing the modules (when reading the
    .bashrc_SIE file), but only when done inside Condor with this job.sh script. I
    have no idea what could be causing this, since we have not changed anything
    related to this recently, but the change has no downsides and it seems to work
    fine, so we change the documentation and tell users to use it.

--- examples/python_bindings/job.sh
+++ examples/python_bindings/job.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Prepare the environment 
-source /usr/local/glob/SIE/.bashrc_SIE
+# Prepare the environment
 export USER=<username>
+export PATH=$PATH
+source /usr/local/glob/SIE/.bashrc_SIE
 
 module load python/3.10  # load modules
 
